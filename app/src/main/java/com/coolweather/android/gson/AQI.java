e20c0249420a0e032634b56e6c212d379fc2681f
Pageo <1206784130@qq.com>
2017-03-12 23:05:24 +0800

    加入天气显示信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by weailily on 2017/3/12 0012.
+ */
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+
+    }
+}
